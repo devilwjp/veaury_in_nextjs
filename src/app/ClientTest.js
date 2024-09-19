6cda7e9a2253a8ea1049ecc2a5b11c3d25adbeac
devilwjp <9700616@qq.com>
2024-09-19 12:13:37 +0800

    feat: support Vue in Next.js

---
+++ src/app/ClientTest.js
@@ -0,0 +1,8 @@
+'use client'
+
+// After being converted by Veaury, the component is a client component
+import { applyVueInReact } from 'veaury'
+import TestVue from './Test.vue'
+const ClientTest = applyVueInReact(TestVue)
+
+export default ClientTest
---
+++ src/app/Test.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="vue-background">
+    <div>This is a Vue Component!</div>
+    <div>I am {{foo}}!</div>
+  </div>
+</template>
+
+<script setup>
+// import './test.css'
+import { ref } from 'vue'
+const foo = ref('Foo')
+</script>
+
+<style scoped>
+@keyframes glow {
+  0% {
+    box-shadow: 0 0 10px #fff;
+  }
+  50% {
+    box-shadow: 0 0 40px #fff, 0 0 40px rgb(0, 255, 108);
+  }
+  100% {
+    box-shadow: 0 0 10px #fff;
+  }
+}
+
+.vue-background {
+  padding: 20px;
+  border-radius: 10px;
+  background: #b0f1cb;
+  animation: glow 1.5s infinite alternate;
+}
+</style>
